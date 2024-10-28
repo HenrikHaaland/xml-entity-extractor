56389e9bb03fd945cf63f82ed34bfe7deb0ac819
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2024-10-28 13:17:53 +0100

    document search function
    you can now search on diffrent documents and id inside thoose documents

--- xee.py
+++ xee.py
@@ -1,29 +1,30 @@
+import sys
+
 def read_xml_file(file_name, entity_id):
 
-    pattern = f"rdf:ID=\"{entity_id}\""
+    id_pattern = f"rdf:ID=\"{entity_id}\""
+
+    about_pattern = f"rdf:about=\"{entity_id}\""
     
-    print(pattern)
+    print(id_pattern)
+    print(about_pattern)
     try:
         with open(file_name) as file:
             line_number = 1
             found_entity = False
-
             end_tag = ""
 
             for line in file:
                 # print(line, end="")
 
-                if pattern in line:
+                if id_pattern in line or about_pattern in line:
                     print(f"id found in line {line_number} {line}")
-
                     found_entity = True
-
-
-
                     tag = line.split()[0]
                     end_tag = tag[1:]
                     end_tag = f"</{end_tag}>"
                     # print(f"Extracted tag: {tag} {end_tag}")
+                    
 
                 if found_entity:
                     print(line, end="")
@@ -37,12 +38,22 @@ def read_xml_file(file_name, entity_id):
 
     except FileNotFoundError: 
         print("file not found")
+print(sys.argv)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python script.py <filename> <entity_id>")
+    else:
+        file_name = sys.argv[1]
+        entity_id = sys.argv[2]
+        read_xml_file(file_name, entity_id)
 
 
+
+"""
 entity_id = input("skriv inn entity id: ")
-
-#read_xml_file("cim.xml")
 
 read_xml_file("cim-id.xml", entity_id)
 
+read_xml_file("cim.xml", entity_id)
+"""
 
