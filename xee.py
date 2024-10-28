db4d2c72139011559c1d899826e1e394918fb7d9
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2024-10-28 13:56:13 +0100

    added output.xml
    added output.xml and cleaned up xee.py

--- xee.py
+++ xee.py
@@ -1,13 +1,16 @@
 import sys
 
+def logg(message):
+    print(message, file=sys.stderr)
+
 def read_xml_file(file_name, entity_id):
 
     id_pattern = f"rdf:ID=\"{entity_id}\""
 
     about_pattern = f"rdf:about=\"{entity_id}\""
     
-    print(id_pattern)
-    print(about_pattern)
+    logg(id_pattern)
+    logg(about_pattern)
     try:
         with open(file_name) as file:
             line_number = 1
@@ -15,17 +18,14 @@ def read_xml_file(file_name, entity_id):
             end_tag = ""
 
             for line in file:
-                # print(line, end="")
-
                 if id_pattern in line or about_pattern in line:
-                    print(f"id found in line {line_number} {line}")
+                    logg(f"id found in line {line_number} {line}")
                     found_entity = True
                     tag = line.split()[0]
                     end_tag = tag[1:]
                     end_tag = f"</{end_tag}>"
                     # print(f"Extracted tag: {tag} {end_tag}")
                     
-
                 if found_entity:
                     print(line, end="")
 
@@ -34,26 +34,16 @@ def read_xml_file(file_name, entity_id):
                     # print("fant slutt tag")
                 line_number = line_number + 1   
 
-            print(f"totalt antal linjer {line_number}")
+            logg(f"totalt antal linjer {line_number}")
 
     except FileNotFoundError: 
-        print("file not found")
-print(sys.argv)
+        logg(f"file not found: {file_name}")
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python script.py <filename> <entity_id>")
+        logg("Usage: python xee.py <filename> <entity_id>")
     else:
         file_name = sys.argv[1]
         entity_id = sys.argv[2]
         read_xml_file(file_name, entity_id)
-
-
-
-"""
-entity_id = input("skriv inn entity id: ")
-
-read_xml_file("cim-id.xml", entity_id)
-
-read_xml_file("cim.xml", entity_id)
-"""
-
