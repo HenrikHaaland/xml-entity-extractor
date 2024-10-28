c5f1beb230f8c7801539ecf1b8ed7dad48526b5a
Henrik Olai Olsbø Håland <156893021+HenrikHaaland@users.noreply.github.com>
2024-10-28 12:24:45 +0100

    finished the extractor
    made it possible to extract segments of the xml code from terminalen

--- xee.py
+++ xee.py
@@ -1,19 +1,43 @@
 def read_xml_file(file_name, entity_id):
 
     pattern = f"rdf:ID=\"{entity_id}\""
+    
     print(pattern)
     try:
-        file = open(file_name)
-        for line in file:
-            #print(line, end="")
+        with open(file_name) as file:
+            line_number = 1
+            found_entity = False
 
-            if pattern in line:
-                print(f"id found in line {line}")
-                
-        file.close()
-        
+            end_tag = ""
+
+            for line in file:
+                # print(line, end="")
+
+                if pattern in line:
+                    print(f"id found in line {line_number} {line}")
+
+                    found_entity = True
+
+
+
+                    tag = line.split()[0]
+                    end_tag = tag[1:]
+                    end_tag = f"</{end_tag}>"
+                    # print(f"Extracted tag: {tag} {end_tag}")
+
+                if found_entity:
+                    print(line, end="")
+
+                if end_tag in line and end_tag !="":
+                    found_entity = False
+                    # print("fant slutt tag")
+                line_number = line_number + 1   
+
+            print(f"totalt antal linjer {line_number}")
+
     except FileNotFoundError: 
         print("file not found")
+
 
 entity_id = input("skriv inn entity id: ")
 
